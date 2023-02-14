c969e59b04e0209294979d24c4fc508dc3983965
Khasan <info.kamalov@gmail.com>
2023-02-14 23:43:51 +0500

    payments with payme

--- api/v1/register/serializers.py
+++ api/v1/register/serializers.py
@@ -8,6 +8,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('id', 'first_name', 'phone_number')
 
     def validate(self, attrs):
-        if len(attrs['phone_number']) != 13 or not attrs['phone_number'].startswith('+'):
+        phone_len = len(attrs['phone_number'])
+        if phone_len > 13 or phone_len < 9:
             raise serializers.ValidationError('Phone number not valid')
         return attrs
